958ccd5e4d15a1a77e7687989a776b9bdf34729a
Charles McQuain <charlesmcquain@gmail.com>
2018-09-11 23:20:35 -0700

    Added template for Chat component

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="chat container">
-    <h2>Chat {{ this.name }}</h2>
+    <h2 class="center teal-text">Ninja Chat</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages">
+          <li>
+            <span class="teal-text">Name</span>
+            <span class="grey-text text-darken-3">Message</span>
+            <span class="grey-text time">Time</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
